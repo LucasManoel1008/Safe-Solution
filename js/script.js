3b7537cda244adb610acf99a42c796e7f1cc1a85
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-04-28 18:39:08 -0300

    Menu lateral

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function abrirNav(){
+    document.getElementById('menuOculto').style.marginLeft = "0";
+    document.getElementById('menuOculto').style.transition = "0.5s";
+
+}
+
+function fecharNav(){
+    document.getElementById('menuOculto').style.marginLeft = "-100vh";
+}
