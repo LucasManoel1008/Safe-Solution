5c2abfd7027be68e66665c2b73df512c265e8d56
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-04-29 18:03:18 -0300

    Menu completo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* Esse primeiro está vinculado ao botão na mesma região da imagem, ele abre o menu.
+    O segundo esta vínculado ao segundo botão, ele fecha o menu.
+    
+    NÃO ALTERAR NADA NESSES DOIS CÓDIGOS*/
 function abrirNav(){
     document.getElementById('menuOculto').style.marginLeft = "0";
     document.getElementById('menuOculto').style.transition = "0.5s";
