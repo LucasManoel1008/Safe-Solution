e35243f7cbdb8105d01773091f11673b8c6b0587
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-05-13 12:22:04 -0300

    Otimização e padronização

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /* Esse primeiro está vinculado ao botão na mesma região da imagem, ele abre o menu.
-    O segundo esta vínculado ao segundo botão, ele fecha o menu.
+    O segundo esta vínculado ao segundo botão, ele fecha o menu.*/
     
-    NÃO ALTERAR NADA NESSES DOIS CÓDIGOS*/
+    window.onscroll = function() {botaoScroll()};
+
+    /*NÃO ALTERAR NADA NESSES DOIS CÓDIGOS*/
 
 function abrirNav(){
     document.getElementById('menuOculto').style.marginLeft = "0";
@@ -11,6 +13,28 @@ function abrirNav(){
 
 function fecharNav(){
     document.getElementById('menuOculto').style.marginLeft = "-100vh";
+}
+
+function botaoScroll(){
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("botaoScroll").style.display = "block";
+        
+      } else {
+        document.getElementById("botaoScroll").style.display = "none";
+        
+      }
+    }
+
+function voltartopo(){
+    var check = 'false'
+    if(check){
+    window.scrollTo({top:0, behavior: 'smooth'})
+    check = 'true'
+    }
+    if(check = false){
+        document.body.scrollTop = 0; // Para navegadores Safari
+        document.documentElement.scrollTop = 0; // Para outros navegadores
+    }
 }
 
     
